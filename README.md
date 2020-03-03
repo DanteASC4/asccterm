8c60fdd87ad26a8414e7c6a6945de940da111fd3
danteasc4 <zexovon@gmail.com>
2020-03-03 10:47:26 -0500

    Wrapping up a couple changes and realized a couple uneeded things oof

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-## Note to self:
-
-
-Left for now:
-- Create second hidden views folder: https://stackoverflow.com/questions/11315351/multiple-view-paths-on-node-js-express
-- Create single page version of the cool vis, add to hidden views folder
-- Set valid cookie, check cookie before accessing secret, if valid render reward secret if not send back to attempt page.
-- Incremenet attempts too
+# Fun project for students!
--- dbGod.js
+++ dbGod.js
@@ -18,9 +18,7 @@ module.exports = new class Dbmaster {
         }
 
         this.addOneEntry();
-        // this.attempt('bob').then(result => {
-        //     console.log(result)
-        // }).catch(e => console.error(e))
+
     }
 
     createTables(){
@@ -28,7 +26,7 @@ module.exports = new class Dbmaster {
     }
 
     /**
-     * adds one entry to the database
+     * Helper method for addEntry method, runs add entry once
      *
      * @return  {void}  returns nothing
      */
@@ -46,7 +44,89 @@ module.exports = new class Dbmaster {
         }
     }
 
+    /**
+     * gets the current number of correct attempts
+     *
+     * @param   {number}    entry_id  which entry's correct attempts to retrieve
+     *
+     * @return  {number}    number of correct attempts
+     */
+    getCorrectCount(entry_id){
+        const sql = `SELECT correct_count FROM answers WHERE id = ?;`;
+        return new Promise((resolve, reject) => {
+            this.db.get(sql, [entry_id], (err, row) => {
+                if(err) return reject(err);
+                return resolve(row.correct_count)
+            })
+        });
+    }
 
+    /**
+     * incremebts correct account
+     *
+     * @param   {number}  id  id of entry to update.
+     *
+     * @return  {bool}    true if successfully updates, or error object if it exists.
+     */
+    async incrementCorrect(id){
+        const sql = `UPDATE answers SET correct_count = ? WHERE id = ?;`;
+        let currentCount = await this.getCorrectCount(id);
+        console.log(`Current count: ${colors.underline.green(currentCount)}`);
+        return new Promise((res, rej) => {
+            this.db.run(sql, [currentCount++, id], function(err) {
+                if(err) rej(err);
+                console.log(`Row(s) updated: ${this.changes}`);
+            })
+        });
+    }
+
+
+    /**
+     * gets the current number of incorrect attempts
+     *
+     * @param   {number}    entry_id  which entry's incorrect attempts to retrieve
+     *
+     * @return  {number}    number of incorrect attempts
+     */
+    getIncorrectCount(entry_id){
+        const sql = `SELECT incorrect_count FROM answers WHERE id = ?;`;
+        return new Promise((resolve, reject) => {
+            this.db.get(sql, [entry_id], (err, row) => {
+                if(err) return reject(err);
+                return resolve(row.incorrect_count)
+            })
+        });
+    }
+
+    /**
+     * incremebts incorrect account
+     *
+     * @param   {number}  id  id of entry to update.
+     *
+     * @return  {bool}    true if successfully updates, or error object if it exists.
+     */
+    async incrementIncorrect(id){
+        const sql = `UPDATE answers SET incorrect_count = ? WHERE id = ?;`;
+        let currentCount = await this.getIncorrectCount(id);
+        console.log(`Current count: ${colors.underline.green(currentCount)}`);
+        return new Promise((res, rej) => {
+            this.db.run(sql, [currentCount++, id], function(err) {
+                if(err) return rej(err);
+                console.log(`Row(s) updated: ${this.changes}`);
+                return res(true)
+            })
+        });
+    }
+
+
+    /**
+     * Function to add an entry to the database
+     *
+     * @param   {string}  p  Password string to be hashed and added to db
+     * @param   {string}  r  Reward string to be added to db
+     *
+     * @return  {void}     returns nothing
+     */
     addEntry(p, r){
         flag(['Add entry 1 ', p, r]);
         const self = this;
@@ -68,6 +148,13 @@ module.exports = new class Dbmaster {
         })
     }
 
+    /**
+     * login attempt method
+     *
+     * @param   {string}  p  password string to check if valid
+     *
+     * @return  {bool}     returns either true or false depending on if the password is valid, if error returns the error object
+     */
     attempt(p){
         const sql = `SELECT id, password FROM answers;`;
         return new Promise((rez, rej) => {
@@ -87,10 +174,13 @@ module.exports = new class Dbmaster {
             }, (reserr, n) =>{
                 flag(['l83, in callback bit', valids])
                 if(reserr) return rej(reserr);
+                console.log(tempRow);
                 if(valids.includes(true)){
+                    this.incrementCorrect(tempRow.id);
                     return rez([true, tempRow]);
                 }
                 else if(valids.includes(false)) {
+                    this.incrementIncorrect(tempRow.id)
                     return rez([false, tempRow]);
                 }
             })
